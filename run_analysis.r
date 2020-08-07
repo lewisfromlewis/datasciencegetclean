3b14345792d1110085289f3168399f904c468508
lewisfromlewis <lewis.campbell@gmail.com>
2020-08-08 00:36:29 +0930

    night

--- run_analysis.r
+++ run_analysis.r
@@ -1,1 +1,5 @@
-train <- read.table("UCI_HAR/train/Inertial Signals/body_acc_x_train.txt", sep = "", quote = "", header=FALSE)
+library(tidyverse)
+
+# Read the data into data frames
+leftrighttrain <- read.table(file="UCI_HAR/train/Inertial Signals/body_acc_x_train.txt", sep = "", quote = "", header=FALSE)
+leftrighttest <- read.table(file="UCI_HAR/test/Inertial Signals/body_acc_x_test.txt", sep = "", quote = "", header=FALSE)
